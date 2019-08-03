fe03217ecea7a326e3edaca7fe14b80ce024ec08
Peter Tang <petersktang@gmail.com>
2019-08-03 23:29:47 +0800

    updated to Swift 5

--- ToDoClearStyle/Class/TDCToDoItemCell.swift
+++ ToDoClearStyle/Class/TDCToDoItemCell.swift
@@ -11,105 +11,105 @@ import UIKit
 class TDCToDoItemCell: UITableViewCell, UITextFieldDelegate {
     
     static let cellId = "ToDoItemCell"
-
+    
     @IBOutlet weak var centerPannel: UIView!
     @IBOutlet weak var leftPannel: UIView!
     @IBOutlet weak var rightPannel: UIView!
-
+    
     @IBOutlet weak var txtField: UITextField!
     @IBOutlet weak var leftLabel: UILabel!
     @IBOutlet weak var rightLabel: UILabel!
     
     @IBOutlet weak var centerConstraint: NSLayoutConstraint!
     private var originConstant: CGFloat = 0
-
+    
     var toDoItem: TDCToDoItem! {
         didSet {
             if let item = toDoItem {
                 txtField.text = item.text
             } else {
                 txtField.text = ""
-                selected = false
+                isSelected = false
             }
         }
     }
-
+    
     var onDelete: ((TDCToDoItemCell) -> Void)?
     var onComplete: ((TDCToDoItemCell) -> Void)?
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-
+        
         leftLabel.text = "\u{2713}"     //对号
         rightLabel.text = "\u{2717}"    //叉号
-
+        
         // add a layer that overlays the cell adding a subtle gradient effect
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = centerPannel.bounds
-        gradientLayer.colors = [UIColor(white: 1, alpha: 0.2).CGColor,
-            UIColor(white: 1, alpha: 0.1).CGColor,
-            UIColor.clearColor().CGColor,
-            UIColor(white: 0, alpha: 0.1).CGColor
+        gradientLayer.colors = [UIColor(white: 1, alpha: 0.2).cgColor,
+                                UIColor(white: 1, alpha: 0.1).cgColor,
+                                UIColor.clear.cgColor,
+                                UIColor(white: 0, alpha: 0.1).cgColor
         ]
-        gradientLayer.locations = [NSNumber(float: 0), NSNumber(float: 0.01), NSNumber(float: 0.95), NSNumber(float: 1)]
-        centerPannel.layer.insertSublayer(gradientLayer, atIndex: 0)
-
+        gradientLayer.locations = [NSNumber(value: 0), NSNumber(value: 0.01), NSNumber(value: 0.95), NSNumber(value: 1)]
+        centerPannel.layer.insertSublayer(gradientLayer, at: 0)
+        
         // 添加Pan手势
-        let recognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
+        let recognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(panGesture:)))
         recognizer.delegate = self
         self.addGestureRecognizer(recognizer)
     }
-
+    
     // 如果是划动手势，仅支持左右划动；如果是其它手势，则有父类负责
-    override func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
+    override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
         if let panGesture = gestureRecognizer as? UIPanGestureRecognizer {
-            let translation = panGesture.translationInView(self.superview)
-            return fabs(translation.x) > fabs(translation.y)
+            let translation = panGesture.translation(in: self.superview)
+            return abs(translation.x) > fabs(translation.y)
         } else {
             return super.gestureRecognizerShouldBegin(gestureRecognizer)
         }
     }
-
+    
     private var deleteOnDragRelease: Bool = false
     private var completeOnDragRelease: Bool = false
-
-    func handlePan(panGesture: UIPanGestureRecognizer) {
+    
+    @objc func handlePan(panGesture: UIPanGestureRecognizer) {
         switch panGesture.state {
-        case .Began:
+        case .began:
             originConstant = centerConstraint.constant
-        case .Changed:
-            let translation = panGesture.translationInView(self)
+        case .changed:
+            let translation = panGesture.translation(in: self)
             centerConstraint.constant = translation.x
-
+            
             // 划动移动1/3宽度为有效划动
-            let finished = fabs(translation.x) > CGRectGetWidth(bounds) / 3
+            let finished = abs(translation.x) > bounds.width / 3
             if translation.x < originConstant { // 右划
                 if finished {
                     deleteOnDragRelease = true
-                    rightLabel.textColor = UIColor.redColor()
+                    rightLabel.textColor = UIColor.red
                 } else {
                     deleteOnDragRelease = false
-                    rightLabel.textColor = UIColor.whiteColor()
+                    rightLabel.textColor = UIColor.white
                 }
             } else { // 左划
                 if finished {
                     completeOnDragRelease = true
-                    leftLabel.textColor = UIColor.greenColor()
+                    leftLabel.textColor = UIColor.green
                 } else {
                     completeOnDragRelease = false
-                    leftLabel.textColor = UIColor.whiteColor()
+                    leftLabel.textColor = UIColor.white
                 }
             }
-        case .Ended:
+        case .ended:
             centerConstraint.constant = originConstant
-
+            
             if deleteOnDragRelease {
                 deleteOnDragRelease = false
                 if let onDelete = onDelete {
                     onDelete(self)
                 }
             }
-
+            
             if completeOnDragRelease {
                 completeOnDragRelease = false
                 if let onComplete = onComplete {
@@ -120,14 +120,14 @@ class TDCToDoItemCell: UITableViewCell, UITextFieldDelegate {
             break
         }
     }
-
+    
     func hideKeyboard() {
         txtField.resignFirstResponder()
         txtField.text = toDoItem.text
     }
     
     // Mark - UITextFieldDelegate
-
+    
     func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
         return !toDoItem.completed
     }
--- ToDoClearStyle/Class/TDCToDoListController.swift
+++ ToDoClearStyle/Class/TDCToDoListController.swift
@@ -11,7 +11,7 @@
 import UIKit
 
 class TDCToDoListController: UITableViewController {
-
+    
     var items = [
         TDCToDoItem(text: "Feed the cat"),
         TDCToDoItem(text: "Buy eggs"),
@@ -29,17 +29,17 @@ class TDCToDoListController: UITableViewController {
         TDCToDoItem(text: "Learn to juggle"),
         TDCToDoItem(text: "Give up")
     ]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let pinch = UIPinchGestureRecognizer(target: self, action: "handlePinch:")
-        let longPress = UILongPressGestureRecognizer(target: self, action: "handleLongPress:")
-
+        
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)) )
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:)) )
+        
         tableView.addGestureRecognizer(pinch)
         tableView.addGestureRecognizer(longPress)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -51,61 +51,61 @@ class TDCToDoListController: UITableViewController {
     }
     
     /*
-    // 简单删除
-    func deleteToDoItem(indexPath: NSIndexPath) {
-        tableView.beginUpdates()
-        items.removeAtIndex(indexPath.row)
-        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-        tableView.endUpdates()
-    }
-    */
-
+     // 简单删除
+     func deleteToDoItem(indexPath: NSIndexPath) {
+     tableView.beginUpdates()
+     items.removeAtIndex(indexPath.row)
+     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+     tableView.endUpdates()
+     }
+     */
+    
     // 更漂亮视觉效果的删除
     func deleteToDoItem(indexPath: NSIndexPath) {
-        let item = items.removeAtIndex(indexPath.row)
+        let item = items.remove(at: indexPath.row)
         var animationEnabled = false
         let lastCell = tableView.visibleCells.last
-        var delay: NSTimeInterval = 0
+        var delay: TimeInterval = 0
         for cell in tableView.visibleCells {
             let cell = cell as! TDCToDoItemCell
             if animationEnabled {
-                UIView.animateWithDuration(0.25, delay: delay, options: .CurveEaseInOut,
-                    animations: { () -> Void in
-                        cell.frame = CGRectOffset(cell.frame, 0, -CGRectGetHeight(cell.frame))
-                    }, completion: { (completed) -> Void in
-                        if cell == lastCell {
-                            self.tableView.reloadData()
-                        }
+                
+                UIView.animate(withDuration: 0.25, delay: delay, options: [.curveEaseInOut], animations: {
+                    cell.frame.offsetBy(dx: 0, dy: -cell.frame.height)
+                }, completion: { (completed) -> Void in
+                    if cell == lastCell {
+                        self.tableView.reloadData()
+                    }
                 })
                 delay += 0.03
             }
-
+            
             if cell.toDoItem == item {
                 animationEnabled = true
-                cell.hidden = true
+                cell.isHidden = true
             }
         }
     }
     
     // MARK: - Pinch
-
+    
     // 插入点
-    private var pinchIndexPath: NSIndexPath?
+    private var pinchIndexPath: IndexPath?
     // 临时代理视图
     private var placheHolderCell: TDCPlaceHolderView?
     // 两触点的起始位置
     private var sourcePoints: (upperPoint: CGPoint, downPoint: CGPoint)?
     // 可以插入操作的标志
     private var pinchInsertEnabled = false
-
-    func handlePinch(pinch: UIPinchGestureRecognizer) {
+    
+    @objc func handlePinch(_ pinch: UIPinchGestureRecognizer) {
         switch pinch.state {
-        case .Began:
-            pinchBegan(pinch)
-        case .Changed:
-            pinchChanged(pinch)
+        case .began:
+            pinchBegan(pinch: pinch)
+        case .changed:
+            pinchChanged(pinch: pinch)
         default:
-            pinchEnd(pinch)
+            pinchEnd(pinch: pinch)
         }
     }
     
@@ -113,27 +113,27 @@ class TDCToDoListController: UITableViewController {
         pinchIndexPath = nil
         sourcePoints = nil
         pinchInsertEnabled = false
-
-        let (upperPoint, downPoint) = pointsOfPinch(pinch)
-        if let upperIndexPath = tableView.indexPathForRowAtPoint(upperPoint),
-            let downIndexPath = tableView.indexPathForRowAtPoint(downPoint) {
-                if downIndexPath.row - upperIndexPath.row == 1 {
-                    let upperCell = tableView.cellForRowAtIndexPath(upperIndexPath)!
-                    let placheHolder = NSBundle.mainBundle().loadNibNamed("TDCPlaceHolderView", owner: tableView, options: nil).first as! TDCPlaceHolderView
-                    placheHolder.frame = CGRectOffset(upperCell.frame, 0, CGRectGetHeight(upperCell.frame) / 2)
-                    tableView.insertSubview(placheHolder, atIndex: 0)
-                    
-                    sourcePoints = (upperPoint, downPoint)
-                    pinchIndexPath = upperIndexPath
-                    placheHolderCell = placheHolder
-                }
+        
+        let (upperPoint, downPoint) = pointsOfPinch(pinch: pinch)
+        if let upperIndexPath = tableView.indexPathForRow(at: upperPoint),
+            let downIndexPath = tableView.indexPathForRow(at: downPoint) {
+            if downIndexPath.row - upperIndexPath.row == 1 {
+                let upperCell = tableView.cellForRow(at: upperIndexPath)!
+                let placheHolder = Bundle.main.loadNibNamed("TDCPlaceHolderView", owner: tableView, options: nil)?.first as! TDCPlaceHolderView
+                placheHolder.frame.offsetBy(dx: 0, dy: upperCell.frame.height / 2)
+                tableView.insertSubview(placheHolder, at: 0)
+                
+                sourcePoints = (upperPoint, downPoint)
+                pinchIndexPath = upperIndexPath
+                placheHolderCell = placheHolder
+            }
         }
     }
-
+    
     func pinchChanged(pinch: UIPinchGestureRecognizer) {
         if let pinchIndexPath = pinchIndexPath, let originPoints = sourcePoints, let placheHolderCell = placheHolderCell {
-            let points = pointsOfPinch(pinch)
-
+            let points = pointsOfPinch(pinch: pinch)
+            
             let upperDistance = points.0.y - originPoints.upperPoint.y
             let downDistance = originPoints.downPoint.y - points.1.y
             let distance = -min(0, min(upperDistance, downDistance))
@@ -141,17 +141,17 @@ class TDCToDoListController: UITableViewController {
             
             // 移动两边的Cell
             for cell in tableView.visibleCells {
-                let indexPath = tableView.indexPathForCell(cell)!
+                let indexPath = tableView.indexPath(for: cell)!
                 if indexPath.row <= pinchIndexPath.row {
-                    cell.transform = CGAffineTransformMakeTranslation(0, -distance)
+                    cell.transform = CGAffineTransform(translationX: 0, y: -distance)
                 } else {
-                    cell.transform = CGAffineTransformMakeTranslation(0, distance)
+                    cell.transform = CGAffineTransform(translationX: 0, y: distance)
                 }
             }
             
             // 插入的Cell变形
-            let scaleY = min(64, fabs(distance) * 2) / CGFloat(64)
-            placheHolderCell.transform = CGAffineTransformMakeScale(1, scaleY)
+            let scaleY = min(64, abs(distance) * 2) / CGFloat(64)
+            placheHolderCell.transform = CGAffineTransform(scaleX: 1, y: scaleY)
             
             placheHolderCell.lblTitle.text = scaleY <= 0.5 ? "张开双指插入新项目": "松手可以插入新项目"
             
@@ -162,98 +162,98 @@ class TDCToDoListController: UITableViewController {
     
     func pinchEnd(pinch: UIPinchGestureRecognizer) {
         if let pinchIndexPath = pinchIndexPath, let placheHolderCell = placheHolderCell {
-            placheHolderCell.transform = CGAffineTransformIdentity
+            placheHolderCell.transform = CGAffineTransform.identity
             placheHolderCell.removeFromSuperview()
             self.placheHolderCell = nil
             
             if pinchInsertEnabled {
                 // 恢复各Cell的transform
                 for cell in self.tableView.visibleCells {
-                    cell.transform = CGAffineTransformIdentity
+                    cell.transform = CGAffineTransform.identity
                 }
-
+                
                 // 插入操作
                 let index = pinchIndexPath.row + 1
-                items.insert(TDCToDoItem(text: ""), atIndex: index)
+                items.insert(TDCToDoItem(text: ""), at: index)
                 tableView.reloadData()
-
+                
                 // 弹出键盘
-                let cell = tableView.cellForRowAtIndexPath(NSIndexPath(forRow: index, inSection: 0)) as! TDCToDoItemCell
+                let cell = tableView.cellForRow(at: IndexPath(row: index, section: 0)) as! TDCToDoItemCell
                 cell.txtField.becomeFirstResponder()
             } else {
                 // 放弃插入，恢复原位置
-                UIView.animateWithDuration(0.25, delay: 0, options: .CurveEaseInOut, animations: { [unowned self] () -> Void in
+                UIView.animate(withDuration: 0.25, delay: 0, options: [.curveEaseInOut], animations: { [unowned self] () -> Void in
                     for cell in self.tableView.visibleCells {
-                        cell.transform = CGAffineTransformIdentity
+                        cell.transform = CGAffineTransform.identity
                     }
                     }, completion: { [unowned self] (completed) -> Void in
                         self.tableView.reloadData()
                 })
             }
         }
-
+        
         sourcePoints = nil
         pinchIndexPath = nil
         pinchInsertEnabled = false
     }
-
+    
     func pointsOfPinch(pinch: UIPinchGestureRecognizer) -> (CGPoint, CGPoint) {
-        if pinch.numberOfTouches() > 1 {
-            let point1 = pinch.locationOfTouch(0, inView: tableView)
-            let point2 = pinch.locationOfTouch(1, inView: tableView)
+        if pinch.numberOfTouches > 1 {
+            let point1 = pinch.location(ofTouch: 0, in: tableView)
+            let point2 = pinch.location(ofTouch: 1, in: tableView)
             if point1.y <= point2.y {
                 return (point1, point2)
             } else {
                 return (point2, point1)
             }
         } else {
-            let point = pinch.locationOfTouch(0, inView: tableView)
+            let point = pinch.location(ofTouch: 0, in: tableView)
             return (point, point)
         }
     }
-
+    
     // MARK: - Drag & Drop
-
-    private var sourceIndexPath: NSIndexPath?
+    
+    private var sourceIndexPath: IndexPath?
     private var snapView: UIView?
-
-    func handleLongPress(longPress: UILongPressGestureRecognizer) {
-        let point = longPress.locationInView(tableView)
-
-        if let indexPath = tableView.indexPathForRowAtPoint(point) {
+    
+    @objc func handleLongPress(_ longPress: UILongPressGestureRecognizer) {
+        let point = longPress.location(in: tableView)
+        
+        if let indexPath = tableView.indexPathForRow(at: point) {
             switch longPress.state {
-            case .Began:
-                if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+            case .began:
+                if let cell = tableView.cellForRow(at: indexPath) {
                     sourceIndexPath = indexPath
-                    let snapView = self.snapView(cell)
+                    let snapView = self.snapView(view: cell)
                     snapView.alpha = 0
-
+                    
                     self.snapView = snapView
-
+                    
                     tableView.addSubview(snapView)
                     
-                    UIView.animateWithDuration(0.25, animations: {
+                    UIView.animate(withDuration: 0.25, animations: {
                         // 选中Cell跳出放大效果
                         snapView.alpha = 0.95
-                        snapView.center = CGPointMake(cell.center.x, point.y)
-                        snapView.transform = CGAffineTransformMakeScale(1.05, 1.05)
-
+                        snapView.center = CGPoint(x: cell.center.x, y: point.y)
+                        snapView.transform = CGAffineTransform(scaleX: 1.05, y: 1.05)
+                        
                         cell.alpha = 0
-                        }, completion: { (completed) -> Void in
-                            cell.hidden = true
-                            cell.alpha = 1
+                    }, completion: { (completed) -> Void in
+                        cell.isHidden = true
+                        cell.alpha = 1
                     })
                 } else {
                     sourceIndexPath = nil
                     snapView = nil
                     break
                 }
-            case .Changed:
+            case .changed:
                 if let snapView = snapView {
                     // 截图随手指上下移动
-                    snapView.center = CGPointMake(snapView.center.x, point.y)
+                    snapView.center = CGPoint(x: snapView.center.x, y: point.y)
                 }
-
+                
                 // 如果手指移动到一个新的Cell上面，隐藏Cell跟此Cell交换位置
                 if let fromIndexPath = sourceIndexPath {
                     if fromIndexPath != indexPath {
@@ -261,16 +261,16 @@ class TDCToDoListController: UITableViewController {
                         let temp = items[indexPath.row]
                         items[indexPath.row] = items[fromIndexPath.row]
                         items[fromIndexPath.row] = temp
-                        tableView.moveRowAtIndexPath(fromIndexPath, toIndexPath: indexPath)
+                        tableView.moveRow(at: fromIndexPath, to: indexPath)
                         tableView.endUpdates()
                         sourceIndexPath = indexPath
                     }
                 }
-
+                
                 // 手指移动到屏幕顶端或底部，UITableView自动滚动
                 let step: CGFloat = 64
                 if let parentView = tableView.superview {
-                    let parentPos = tableView.convertPoint(point, toView: parentView)
+                    let parentPos = tableView.convert(point, to: parentView)
                     if parentPos.y > parentView.bounds.height - step {
                         var offset = tableView.contentOffset
                         offset.y += (parentPos.y - parentView.bounds.height + step)
@@ -288,76 +288,76 @@ class TDCToDoListController: UITableViewController {
                     }
                 }
             default:
-                if let snapView = snapView, let fromIndexPath = sourceIndexPath, let cell = tableView.cellForRowAtIndexPath(fromIndexPath) {
+                if let snapView = snapView, let fromIndexPath = sourceIndexPath, let cell = tableView.cellForRow(at: fromIndexPath) {
                     cell.alpha = 0
-                    cell.hidden = false
-
+                    cell.isHidden = false
+                    
                     // 长按移动结束，隐藏的Cell恢复显示，删除截图
-                    UIView.animateWithDuration(0.25, animations: { () -> Void in
+                    UIView.animate(withDuration: 0.25, animations: { () -> Void in
                         snapView.center = cell.center
                         snapView.alpha = 0
                         
                         cell.alpha = 1
-                        }, completion: { [unowned self] (completed) -> Void in
-                            snapView.removeFromSuperview()
-                            self.snapView = nil
-                            self.sourceIndexPath = nil
-
-                            self.tableView.performSelector("reloadData", withObject: nil, afterDelay: 0.5)
+                    }, completion: { [unowned self] (completed) -> Void in
+                        snapView.removeFromSuperview()
+                        self.snapView = nil
+                        self.sourceIndexPath = nil
+                        
+                        self.tableView.perform(#selector(self.tableView.reloadData), with: nil, afterDelay: 0.5)
                     })
                 }
             }
         }
     }
-
+    
     // UIView截图为UIImageView
     func snapView(view: UIView) -> UIImageView {
         UIGraphicsBeginImageContextWithOptions(view.bounds.size, false, 0)
-        view.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
         let snapShot = UIImageView(image: image)
         snapShot.layer.masksToBounds = false;
         snapShot.layer.cornerRadius = 0;
-        snapShot.layer.shadowOffset = CGSizeMake(-5.0, 0.0);
+        snapShot.layer.shadowOffset = CGSize(width: -5.0, height: 0.0);
         snapShot.layer.shadowOpacity = 0.4;
         snapShot.layer.shadowRadius = 5;
         snapShot.frame = view.frame
         return snapShot
     }
-
+    
     // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(TDCToDoItemCell.cellId, forIndexPath: indexPath) as! TDCToDoItemCell
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TDCToDoItemCell.cellId, for: indexPath) as! TDCToDoItemCell
         let toDoItem = items[indexPath.row]
         cell.toDoItem = toDoItem
-        cell.centerPannel.backgroundColor = toDoItem.completed ? UIColor(red: 0, green: 0.6, blue: 0, alpha: 1): backgroundColor(indexPath)
-
+        cell.centerPannel.backgroundColor = toDoItem.completed ? UIColor(red: 0, green: 0.6, blue: 0, alpha: 1): backgroundColor(indexPath: NSIndexPath(item: indexPath.row, section: indexPath.section))
+        
         cell.onDelete = { [unowned self] cell in
-            if let indexPath = tableView.indexPathForCell(cell) {
-                self.deleteToDoItem(indexPath)
+            if let indexPath = tableView.indexPath(for: cell) {
+                self.deleteToDoItem(indexPath: NSIndexPath(item: indexPath.row, section: indexPath.section))
             }
         }
-
+        
         cell.onComplete = { [unowned self] cell in
-            if let indexPath = tableView.indexPathForCell(cell) {
+            if let indexPath = tableView.indexPath(for: cell) {
                 let toDoItem = self.items[indexPath.row]
                 toDoItem.completed = !toDoItem.completed
-                tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+                tableView.reloadRows(at: [indexPath], with: .automatic)
             }
         }
-
+        
         return cell
     }
-
+    
 }
